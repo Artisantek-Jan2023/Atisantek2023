561893f82558d75f67325c26ff031fb92ef6a770
jagadeesh <jagginb@gmail.com>
2023-02-14 13:46:52 +0000

    shell script added

---
+++ shell/rangeeo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter the value of n"
+read n
+
+for i in $(seq 0 $n)
+do
+    if (( i%2==0 ))
+    then
+        echo "$i is even"
+    else
+        echo "$i is odd"
+    fi
+done
