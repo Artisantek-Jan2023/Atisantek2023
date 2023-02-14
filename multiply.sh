8a20957407a28c40776de742030cefb96c1332e4
Cloud User <ec2-user@ip-172-31-7-23.ap-south-1.compute.internal>
2023-02-14 13:54:00 +0000

    commit shell script

---
+++ multiply.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+
+product=0
+
+while [ $product -lt $num2 ]
+do
+        product=$(($product + $num1))
+        n=$((n++))
+done
+
+echo "The product of numbers is $product"
